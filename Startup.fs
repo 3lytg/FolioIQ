49539afe8adb0947f638e93c5e38926496cc19fe
Alaeddine Sliti <3lytg@users.noreply.github.com>
2026-03-09 21:14:00 +0000

    wire up database init on startup

---
+++ Startup.fs
@@ -0,0 +1,31 @@
+namespace FolioIQ
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open WebSharper.AspNetCore
+
+module Program =
+    [<EntryPoint>]
+    let main args =
+        let builder = WebApplication.CreateBuilder(args)
+        
+        builder.Services.AddWebSharper()
+            .AddAuthentication("WebSharper")
+            .AddCookie("WebSharper", fun options -> ())
+            |> ignore
+
+        let app = builder.Build()
+
+        if app.Environment.IsDevelopment() then
+            app.UseDeveloperExceptionPage() |> ignore
+
+        app.UseStaticFiles()
+           .UseAuthentication()
+           .UseWebSharper()
+           |> ignore
+
+        app.Run()
+        0
+
+// working on wire up database init on startup...
